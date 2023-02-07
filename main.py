d14d7c4d5388d5b2b3a64dbdeef43e1fc5b875c2
Erikpup <102089873+Erikpup@users.noreply.github.com>
2023-02-07 14:55:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-import os
-
-import requests
 import telebot
 from confing import *
-from flask import Flask,request
 
-bot = telebot.TeleBot(BOT_TELEGRAM)
-server = Flask(__name__)
 
-@bot.message_handler(commands=["start"])
-def start(m):
-    username = m.from_user.username
-    bot.reply_to(m, f'Hello, {username}')
+bot = telebot.TeleBot(TOKEN)
 
-@server.route(f'/{BOT_TELEGRAM}',methods={'POST'})
-def redirect_message():
-    json_str = request.get_data().decode('utf-8')
-    update=telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '!',200
 
-if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+                     'Привет! Я эхо-бот, пересылающий слова от пользователя. Просто напиши мне что-нибудь!')
+
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.polling()
