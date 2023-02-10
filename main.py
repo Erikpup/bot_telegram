f3c709c892aed4ed4c9e54cba40820527b207e58
Erikpup <102089873+Erikpup@users.noreply.github.com>
2023-02-10 17:52:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import telebot
-from confing import *
-
-
-bot = telebot.TeleBot(TOKEN)
-
+import confing
+bot = telebot.TeleBot(confing.TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id,
-                     'Привет! Я эхо-бот, пересылающий слова от пользователя. Просто напиши мне что-нибудь!')
+def send_welcome(message):
+    bot.reply_to(message, "Привет, давай поговорим")
 
-
-@bot.message_handler(content_types=['text'])
-def send_echo(message):
-    bot.send_message(message.chat.id, message.text)
-
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    if 'привет' in str(message.text).lower():
+        bot.reply_to(message, "Привет, я пишу тебе из облака")
+    else:
+        bot.reply_to(message, f"Создатель меня еще не научил отвечать на '{message.text}'")
 
 bot.polling()
